32541937d065935b69eca8359e566fe320310603
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 12:58:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,74 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a.size < b.size
+      -1
+    elsif a.size > b.size
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
+  array
+end
+
+def reverse_array(array)
+  newArray = []
+  array.each do |element|
+    newArray.unshift(element)
+  end
+  newArray
+end
+
+def kesha_maker(array)
+  newArray = []
+  array.each do |element|
+    element[2] = "$"
+    newArray.push(element)
+  end
+  newArray
+end
+
+def find_a(array)
+  newArray = []
+  array.each do |element|
+    if element.start_with?("a")
+      newArray.push(element)
+    end
+  end
+  newArray
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index do |text, index|
+    if index != 1
+      array = text << "s"
+    end
+  end
+end
